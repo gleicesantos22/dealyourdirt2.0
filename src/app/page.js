05fd421747af47ce515344cbd1d1e7acf78b9e5d
Tanvir Ahamed <sowmik265@gmail.com>
2025-05-27 23:48:58 +0600

    full code

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import ProductGrid from "@/components/ProductGird";
+import ValentineMarquee from "@/components/ValentineMarquee";
+
+export default function Home() {
+  return (
+    <div>
+      <ProductGrid></ProductGrid>
+      <ValentineMarquee></ValentineMarquee>
+    </div>
+  );
+}
